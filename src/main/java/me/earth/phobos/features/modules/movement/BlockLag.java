9c44f55e28f49e27cc303576d8061d0f9fbfadc9
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-05-31 03:38:00 -0700

    Tiny Cleanup of bush's burrow :flushed:
    xd.

--- src/main/java/me/earth/phobos/features/modules/movement/BlockLag.java
+++ src/main/java/me/earth/phobos/features/modules/movement/BlockLag.java
@@ -5,20 +5,22 @@ import me.earth.phobos.event.events.UpdateWalkingPlayerEvent;
 import me.earth.phobos.features.command.Command;
 import me.earth.phobos.features.modules.Module;
 import me.earth.phobos.features.setting.Setting;
-import me.earth.phobos.util.*;
+import me.earth.phobos.util.BlockUtil;
+import me.earth.phobos.util.InventoryUtil;
+import me.earth.phobos.util.MathUtil;
 import net.minecraft.block.BlockEnderChest;
 import net.minecraft.block.BlockObsidian;
 import net.minecraft.block.BlockSoulSand;
-import net.minecraft.network.play.client.CPacketPlayer;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.*;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
+import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public
 class BlockLag
@@ -27,16 +29,16 @@ class BlockLag
 
     private final Setting < Mode > mode = this.register ( new Setting < Mode > ( "Mode" , Mode.OBSIDIAN ) );
     private final Setting < Boolean > smartTp = this.register ( new Setting < Boolean > ( "SmartTP" , true ) );
-    private final Setting < Integer > tpMin = this.register ( new Setting < Integer > ( "TPMin" , 4 , 3 , 10 , v -> this.smartTp.getValue ( ) ) );
+    private final Setting < Integer > tpMin = this.register ( new Setting < Integer > ( "TPMin" , 3 , 3 , 10 , v -> this.smartTp.getValue ( ) ) );
     private final Setting < Integer > tpMax = this.register ( new Setting < Integer > ( "TPMax" , 25 , 10 , 40 , v -> this.smartTp.getValue ( ) ) );
-    private final Setting < Boolean > noVoid = this.register ( new Setting < Boolean > ( "NoVoid" , true, v -> this.smartTp.getValue ( ) ) );
-    private final Setting < Integer > tpHeight = this.register ( new Setting < Integer > ( "TPHeight" , 25 , -40 , 40 , v -> ! this.smartTp.getValue ( ) ) );
-    private final Setting < Boolean > keepInside = this.register ( new Setting < Boolean > ( "KeepInside" , true ) );
+    private final Setting < Boolean > noVoid = this.register ( new Setting < Boolean > ( "NoVoid" , true , v -> this.smartTp.getValue ( ) ) );
+    private final Setting < Integer > tpHeight = this.register ( new Setting < Integer > ( "TPHeight" , 2 , - 40 , 40 , v -> ! this.smartTp.getValue ( ) ) );
+    private final Setting < Boolean > keepInside = this.register ( new Setting < Boolean > ( "Center" , true ) );
     private final Setting < Boolean > rotate = this.register ( new Setting < Boolean > ( "Rotate" , false ) );
     private final Setting < Boolean > sneaking = this.register ( new Setting < Boolean > ( "Sneak" , false ) );
     private final Setting < Boolean > offground = this.register ( new Setting < Boolean > ( "Offground" , false ) );
     private final Setting < Boolean > chat = this.register ( new Setting < Boolean > ( "Chat Msgs" , true ) );
-    private final Setting < Boolean > tpdebug = this.register ( new Setting < Boolean > ( "Debug" , false, v -> this.chat.getValue ( ) && this.smartTp.getValue ( ) ) );
+    private final Setting < Boolean > tpdebug = this.register ( new Setting < Boolean > ( "Debug" , false , v -> this.chat.getValue ( ) && this.smartTp.getValue ( ) ) );
 
     private BlockPos burrowPos;
     private int lastBlock;
@@ -103,13 +105,13 @@ class BlockLag
         }
 
         if ( this.rotate.getValue ( ) ) {
-            float[] angle = MathUtil.calcAngle(BlockLag.mc.player.getPositionEyes(mc.getRenderPartialTicks()), new Vec3d((float) burrowPos.getX() + 0.5f, (float) burrowPos.getY() + 0.5f, (float) burrowPos.getZ() + 0.5f));
-            Phobos.rotationManager.setPlayerRotations(angle[0], angle[1]);
+            float[] angle = MathUtil.calcAngle ( BlockLag.mc.player.getPositionEyes ( mc.getRenderPartialTicks ( ) ) , new Vec3d ( (float) burrowPos.getX ( ) + 0.5f , (float) burrowPos.getY ( ) + 0.5f , (float) burrowPos.getZ ( ) + 0.5f ) );
+            Phobos.rotationManager.setPlayerRotations ( angle[0] , angle[1] );
         }
 
         InventoryUtil.switchToHotbarSlot ( this.blockSlot , false );
         BlockUtil.placeBlock ( this.burrowPos , this.blockSlot == - 2 ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND , false , true , this.sneaking.getValue ( ) );
-        InventoryUtil.switchToHotbarSlot( this.lastBlock , false );
+        InventoryUtil.switchToHotbarSlot ( this.lastBlock , false );
 
         BlockLag.mc.player.connection.sendPacket ( new CPacketPlayer.Position ( BlockLag.mc.player.posX , ( this.smartTp.getValue ( ) ? this.adaptiveTpHeight ( ) : this.tpHeight.getValue ( ) + BlockLag.mc.player.posY ) , BlockLag.mc.player.posZ , ! this.offground.getValue ( ) ) );
 
@@ -120,8 +122,8 @@ class BlockLag
     int findBlockSlot ( ) {
 
         // theres prob an easier way to do this but im retarded
-        // bobos uses "this." everywhere so im just gonna keep doing it even though its kinda pointless
-        switch ( this.mode.getValue ( ) ) {
+        // phobos uses "this." everywhere so im just gonna keep doing it even though its kinda pointless
+        switch (this.mode.getValue ( )) {
             case ECHEST:
                 this.block = BlockEnderChest.class;
                 this.name = "Ender Chests";
@@ -141,7 +143,8 @@ class BlockLag
             if ( InventoryUtil.isBlock ( BlockLag.mc.player.getHeldItemOffhand ( ).getItem ( ) , this.block ) ) {
                 return - 2;
             } else {
-                if ( this.chat.getValue ( ) ) Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c No " + this.name + " to use" );
+                if ( this.chat.getValue ( ) )
+                    Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c No " + this.name + " to use" );
             }
         }
 
@@ -154,7 +157,7 @@ class BlockLag
         int airblock = ( this.noVoid.getValue ( ) && this.tpMax.getValue ( ) * - 1 + this.burrowPos.getY ( ) < 0 ? this.burrowPos.getY ( ) * - 1 : this.tpMax.getValue ( ) * - 1 );
 
         while ( airblock < this.tpMax.getValue ( ) ) {
-            if ( Math.abs ( airblock ) < this.tpMin.getValue ( ) || ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP, airblock ) ) || ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP, airblock + 1 ) ) ) {
+            if ( Math.abs ( airblock ) < this.tpMin.getValue ( ) || ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP , airblock ) ) || ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP , airblock + 1 ) ) ) {
                 airblock++;
             } else {
                 if ( this.tpdebug.getValue ( ) ) Command.sendMessage ( Integer.toString ( airblock ) );
@@ -165,14 +168,16 @@ class BlockLag
         return 69420; // if there isn't any room
     }
 
-    private boolean
+    private
+    boolean
     doChecks ( ) {
 
         if ( ! BlockLag.fullNullCheck ( ) ) {
 
             if ( this.smartTp.getValue ( ) ) {
                 if ( adaptiveTpHeight ( ) == 69420 ) { // check if there is room to tp
-                    if ( this.chat.getValue ( ) ) Command.sendMessage ( "\u00A77" + this.displayName.getValue() + ":\u00A7c Not enough room" );
+                    if ( this.chat.getValue ( ) )
+                        Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c Not enough room" );
                     return false;
                 }
             }
@@ -181,15 +186,17 @@ class BlockLag
                 return false;
             }
 
-            if ( ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP, 2 ) ) ) { // is the player trapped
-                if ( this.chat.getValue ( ) ) Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c Not enough room");
+            if ( ! BlockLag.mc.world.isAirBlock ( this.burrowPos.offset ( EnumFacing.UP , 2 ) ) ) { // is the player trapped
+                if ( this.chat.getValue ( ) )
+                    Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c Not enough room" );
                 return false;
             }
 
-            for ( final Entity entity : BlockLag.mc.world.loadedEntityList ) {
+            for (final Entity entity : BlockLag.mc.world.loadedEntityList) {
                 if ( ! ( entity instanceof EntityItem ) && ! entity.equals ( BlockLag.mc.player ) ) {
                     if ( new AxisAlignedBB ( burrowPos ).intersects ( entity.getEntityBoundingBox ( ) ) ) { // is there another player in the hole
-                        if ( this.chat.getValue ( ) ) Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c Not enough room");
+                        if ( this.chat.getValue ( ) )
+                            Command.sendMessage ( "\u00A77" + this.displayName.getValue ( ) + ":\u00A7c Not enough room" );
                         return false;
                     }
                 }
